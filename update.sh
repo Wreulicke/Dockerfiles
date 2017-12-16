7503c02a7ed8f5aa0692c120ef70c434977c97be
wreulicke <saito.masaya@classmethod.jp>
2017-12-17 02:10:17 +0900

    add shell

---
+++ update.sh
@@ -0,0 +1,2 @@
+# !/bin/sh
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/wreulicke/circleci/trigger/891e084b-58d2-4816-87a2-faee57791375/
